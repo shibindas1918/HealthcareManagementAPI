335e7798fe10d5e688ccb88dd952de8542fdd7d1
sibin <sibindas1806@gmail.com>
2024-12-17 14:53:03 +0530

    Added few controller commented lines for better readablity

--- HealthcareManagementAPI/Services/PatientService.cs
+++ HealthcareManagementAPI/Services/PatientService.cs
@@ -2,6 +2,7 @@
 using HealthcareManagementAPI.Models;
 using System.Data;
 using Dapper;
+using System.Threading.Tasks;
 
 namespace HealthcareManagementAPI.Services
 {
@@ -13,31 +14,31 @@ namespace HealthcareManagementAPI.Services
         {
             _dbConnection = dbConnection;
         }
-
+       //Method to get all patients 
         public async Task<IEnumerable<Patient>> GetAllPatients()
         {
             string query = "SELECT * FROM Patients";
             return await _dbConnection.QueryAsync<Patient>(query);
         }
-
+        //Method to get a particular patient by Id 
         public async Task<Patient> GetPatientById(int id)
         {
             string query = "SELECT * FROM Patients WHERE PatientId = @Id";
             return await _dbConnection.QueryFirstOrDefaultAsync<Patient>(query, new { Id = id });
         }
-
+        //Method to Create a patients
         public async Task AddPatient(Patient patient)
         {
             string query = "INSERT INTO Patients (Name, Age, Gender, Contact) VALUES (@Name, @Age, @Gender, @Contact)";
             await _dbConnection.ExecuteAsync(query, patient);
         }
-
+        //Method to update a particular patient
         public async Task UpdatePatient(Patient patient)
         {
             string query = "UPDATE Patients SET Name = @Name, Age = @Age, Gender = @Gender, Contact = @Contact WHERE PatientId = @PatientId";
             await _dbConnection.ExecuteAsync(query, patient);
         }
-
+        //Method to delete a particular patient
         public async Task DeletePatient(int id)
         {
             string query = "DELETE FROM Patients WHERE PatientId = @Id";
